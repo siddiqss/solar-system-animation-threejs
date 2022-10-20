368d48dcae5a10e3ca989aea3b722f9013ee4717
talha <tstalhasiddique@gmail.com>
2022-10-20 20:11:52 +0500

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-import * as THREE from "three";
+import * as THREE from "https://unpkg.com/three@0.144.0/build/three.module.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 import starsTexture from "../img/stars.jpg";
